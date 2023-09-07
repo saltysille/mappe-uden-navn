9f6047e7cced839a40ee14db025b775f1da0275c
Sille <cekj0002@stud.kea.dk>
2023-09-07 14:42:17 +0200

    nav menu

--- burger.js
+++ burger.js
@@ -1,4 +1,16 @@
-document.querySelector(".burger-menu").addEventListener("click", function () {
-  this.classList.toggle("active");
-  document.querySelector(".menu").classList.toggle("active");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+const links = document.querySelectorAll(".menu li a");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    burger.classList.remove("active");
+    nav.classList.remove("active");
+  });
 });
