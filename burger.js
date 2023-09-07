b95cc61bffc9cac8096f4c7eabe1a7dfa76aa6b6
Sille <cekj0002@stud.kea.dk>
2023-09-07 14:19:06 +0200

    main

---
+++ burger.js
@@ -0,0 +1,4 @@
+document.querySelector(".burger-menu").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".menu").classList.toggle("active");
+});
