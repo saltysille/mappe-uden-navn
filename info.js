3b86cacb6ff18bdb614147be77b99fff3ef39270
Emilia <empa0001@stud.kea.dk>
2023-09-11 10:54:13 +0200

    js

---
+++ info.js
@@ -0,0 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((respnse) => respnse.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("p span").textContent = product.price;
+  document.querySelector(".description").textContent = product.description;
+}
