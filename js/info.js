d419bdbde49f67ab31cec6d16ce1c65f89929316
Sille <cekj0002@stud.kea.dk>
2023-09-12 20:44:50 +0200

    sille java

--- js/info.js
+++ js/info.js
@@ -1,7 +1,9 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-const url = "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?id=eq." + id;
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
+const url =
+  "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?id=eq." + id;
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
 
 const options = {
   method: "GET",
@@ -10,8 +12,8 @@ const options = {
   },
 };
 
-fetch(url + id, options)
-  .then((respnse) => respnse.json())
+fetch(url, options)
+  .then((response) => response.json())
   .then((data) => visMad(data));
 
 function visMad(item) {
--- js/liste.js
+++ js/liste.js
@@ -1,8 +1,10 @@
 // Get the query parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("kategori");
-const url = "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?kategori=eq.";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
+const url =
+  "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?kategori=eq.";
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
 
 console.log(category);
 
@@ -32,7 +34,9 @@ function visMad(mad) {
     // Replace these with actual property names from your data
     copy.querySelector("img").src = item.illustrations;
     copy.querySelector("h3").textContent = item.title;
-    copy.querySelector("img").setAttribute("href", `info-side.html?id=${item.id}`);
+    copy
+      .querySelector("img")
+      .setAttribute("href", `info-side.html?id=${item.illustrations}`);
     document.querySelector("main").appendChild(copy);
   });
 }
