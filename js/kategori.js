3400e8dd22d711dcf2dc2a1a79e1967aa84f5eb3
Emilia <empa0001@stud.kea.dk>
2023-09-12 10:02:56 +0200

    js

--- js/kategori.js
+++ js/kategori.js
@@ -1,12 +1,28 @@
-/// https://szmeszolzqvldfshrvhu.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ
+/* const imageElements = document.querySelectorAll(".img_forside");
 
-fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
+imageElements.forEach((img) => {
+  img.addEventListener("click", handleImageClick);
+});
+
+function handleImageClick(event) {
+  // Get the clicked image element
+  const clickedImage = event.target;
+
+  console.log(event);
+
+  // Extract data from the clicked image (for example, alt attribute)
+  const imageData = {
+    alt: clickedImage.alt,
+    src: clickedImage.src,
+  };
+  fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
   },
 })
+}
+
   .then((res) => res.json())
   .then(showCategories);
 
@@ -15,15 +31,28 @@ function showCategories(cats) {
   cats.forEach(showCategory);
 }
 
-function showCategory(cat) {
-  console.log(cat);
-  //fanger
-  const template = document.querySelector("template").content;
-  //cloner
-  const clone = template.cloneNode(true);
-  //ændre indhold
-  clone.querySelector("img").src = cat.illustrations;
-  clone.querySelector("img").href = `index.html?category=${cat.category}`;
-  //appender
-  document.querySelector(".letterGroup ol").appendChild(clone);
-}
+function showProducts(products) {
+  products.forEach(showProduct);
+} */
+// Get all the image elements with class "img_forside"
+const imageElements = document.querySelectorAll(".img_forside");
+
+// Add a click event listener to each image
+imageElements.forEach((img) => {
+  img.addEventListener("click", function () {
+    // Determine which image was clicked by its alt attribute
+    const altText = img.alt;
+    console.log(img);
+
+    // Redirect to "liste.html" and pass the alt text as a query parameter
+    window.location.href = `liste.html?image=${encodeURIComponent(altText)}`;
+  });
+});
+fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
+  },
+})
+  .then((res) => res.json())
+  .then(showCategories);
--- js/liste.js
+++ js/liste.js
@@ -1,19 +1,45 @@
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
+// /// https://szmeszolzqvldfshrvhu.supabase.co
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ
 
-if (category) {
-  fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?select=categories/0/name" + category)
-    .then((response) => response.json())
-    .then(showProducts);
-} else {
-  fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad")
-    .then((response) => response.json())
-    .then(showProducts);
-}
+// fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
+//   method: "GET",
+//   headers: {
+//     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
+//   },
+// })
+//   .then((res) => res.json())
+//   .then(showCategories);
 
-function showProducts(products) {
-  products.forEach(showProduct);
-}
+// function showCategories(cats) {
+//   console.log(cats);
+//   cats.forEach(showCategory);
+// }
+
+// function showCategory(cat) {
+//   console.log(cat);
+//   //fanger
+//   const template = document.querySelector("template").content;
+//   //cloner
+//   const clone = template.cloneNode(true);
+//   //ændre indhold
+//   clone.querySelector("img").src = cat.illustrations;
+//   clone.querySelector("img").href = `index.html?category=${cat.category}`;
+//   //appender
+//   document.querySelector(".letterGroup ol").appendChild(clone);
+// }
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const category = urlParams.get("category");
+
+// if (category) {
+//   fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?select=categories/0/name" + category)
+//     .then((response) => response.json())
+//     .then(showProducts);
+// } else {
+//   fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad")
+//     .then((response) => response.json())
+//     .then(showProducts);
+// }
 
 function showProduct(product) {
   const template = document.querySelector("#stiTemplate").content;
