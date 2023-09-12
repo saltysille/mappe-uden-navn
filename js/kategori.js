ead64c2fe47d0d3d30dafd857d814facfd3fed13
Emilia <empa0001@stud.kea.dk>
2023-09-12 10:41:34 +0200

    js

--- js/kategori.js
+++ js/kategori.js
@@ -1,39 +1,3 @@
-/* const imageElements = document.querySelectorAll(".img_forside");
-
-imageElements.forEach((img) => {
-  img.addEventListener("click", handleImageClick);
-});
-
-function handleImageClick(event) {
-  // Get the clicked image element
-  const clickedImage = event.target;
-
-  console.log(event);
-
-  // Extract data from the clicked image (for example, alt attribute)
-  const imageData = {
-    alt: clickedImage.alt,
-    src: clickedImage.src,
-  };
-  fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
-  method: "GET",
-  headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
-  },
-})
-}
-
-  .then((res) => res.json())
-  .then(showCategories);
-
-function showCategories(cats) {
-  console.log(cats);
-  cats.forEach(showCategory);
-}
-
-function showProducts(products) {
-  products.forEach(showProduct);
-} */
 // Get all the image elements with class "img_forside"
 const imageElements = document.querySelectorAll(".img_forside");
 
@@ -44,11 +8,16 @@ imageElements.forEach((img) => {
     const altText = img.alt;
     console.log(img);
 
-    // Redirect to "liste.html" and pass the alt text as a query parameter
-    window.location.href = `liste.html?image=${encodeURIComponent(altText)}`;
+    // Determine the category based on the clicked image (adjust this logic as needed)
+    let category = "";
+
+    // Redirect to "liste.html" and pass the alt text and category as query parameters
+    window.location.href = `liste.html?category=${img.category}`;
+
+    document.querySelector(".wrapper img").appendChild(clone);
   });
 });
-fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
+fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad/categories/0/name", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
@@ -56,3 +25,12 @@ fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
 })
   .then((res) => res.json())
   .then(showCategories);
+
+function showCategories(cats) {
+  console.log(cats);
+  cats.forEach(showCategory);
+}
+
+function showProducts(products) {
+  products.forEach(showProduct);
+}
--- js/liste.js
+++ js/liste.js
@@ -1,52 +1,28 @@
-// /// https://szmeszolzqvldfshrvhu.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ
+// Get the query parameters from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get(category);
 
-// fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad", {
-//   method: "GET",
-//   headers: {
-//     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ",
-//   },
-// })
-//   .then((res) => res.json())
-//   .then(showCategories);
+console.log(category);
 
-// function showCategories(cats) {
-//   console.log(cats);
-//   cats.forEach(showCategory);
-// }
+if (category) {
+  // Fetch data from your JSON source (similar to your previous code)
+  fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?select=categories/0/name/", {
+    method: "GET",
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ", // Replace with your actual API key
+    },
+  })
+    .then((response) => response.json())
+    .then(showProducts);
+}
 
-// function showCategory(cat) {
-//   console.log(cat);
-//   //fanger
-//   const template = document.querySelector("template").content;
-//   //cloner
-//   const clone = template.cloneNode(true);
-//   //ændre indhold
-//   clone.querySelector("img").src = cat.illustrations;
-//   clone.querySelector("img").href = `index.html?category=${cat.category}`;
-//   //appender
-//   document.querySelector(".letterGroup ol").appendChild(clone);
-// }
-
-// const urlParams = new URLSearchParams(window.location.search);
-// const category = urlParams.get("category");
-
-// if (category) {
-//   fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?select=categories/0/name" + category)
-//     .then((response) => response.json())
-//     .then(showProducts);
-// } else {
-//   fetch("https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad")
-//     .then((response) => response.json())
-//     .then(showProducts);
-// }
+function showProducts(products) {
+  products.forEach(showProduct);
+}
 
 function showProduct(product) {
   const template = document.querySelector("#stiTemplate").content;
   const copy = template.cloneNode(true);
-
-  copy.querySelector("img").src = `https://marieelfrida.com/kea/assets/${product.id}.svg`;
-
-  copy.querySelector(".read-more").setAttribute("href", `info.html?id=${product.id}`);
-  document.querySelector("main").appendChild(copy);
+  //ændre indhold
+  copy.querySelector("h3").textContent = product.productdisplayname;
 }
