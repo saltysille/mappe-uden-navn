46d2abfea87f6f3200441ba355efdab9722a54b8
Emilia <empa0001@stud.kea.dk>
2023-09-13 17:03:54 +0200

    kommentar

--- js/liste.js
+++ js/liste.js
@@ -1,7 +1,10 @@
-// her henter vi dataen - og vælger at hente kategorierne
+// her opretter vi en URLParams
 const urlParams = new URLSearchParams(window.location.search);
+// vi henter værdien kategori
 const category = urlParams.get("kategori");
+// vi filtrer daten for at hente en bestemt kategori
 const url = "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?kategori=eq.";
+// vi bruger key variablen til vores API
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
 
 console.log(category);
@@ -24,9 +27,10 @@ function visMad(mad) {
   mad.forEach((item) => {
     const template = document.querySelector("#stiTemplate").content;
     const copy = template.cloneNode(true);
-    //ændre indhold
+    //ændre indhold / opdatere indholdet at html'en for indholdet baseret på dataten
     copy.querySelector("img").src = item.illustrations;
     copy.querySelector("h3").textContent = item.title;
+    //Vi opdatere senere linkets attribut med et a-tag for at pege på en bestemt side -baseret på dataens id
     copy.querySelector("a").setAttribute("href", `info-side.html?id=${item.id}`);
     document.querySelector("main").appendChild(copy);
   });
