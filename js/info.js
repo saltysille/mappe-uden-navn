cccaf7f133438851c4cf6b1da2f1aed5f8a3ba1d
Emilia <empa0001@stud.kea.dk>
2023-09-13 10:54:40 +0200

    js

--- js/info.js
+++ js/info.js
@@ -1,9 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
-const url =
-  "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?id=eq." + id;
-const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
+const url = "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?id=eq." + id;
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
 
 const options = {
   method: "GET",
@@ -18,6 +16,6 @@ fetch(url, options)
 
 function visMad(item) {
   console.log(item);
-  document.querySelector("h3").textContent = item.title;
-  document.querySelector("img").src = item.illustrations;
+  document.querySelector(".background-info h3").textContent = item.title;
+  document.querySelector(".img-info").src = item.illustrations;
 }
--- js/liste.js
+++ js/liste.js
@@ -1,10 +1,8 @@
 // Get the query parameters from the URL
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("kategori");
-const url =
-  "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?kategori=eq.";
-const key =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
+const url = "https://szmeszolzqvldfshrvhu.supabase.co/rest/v1/vildmad?kategori=eq.";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bWVzem9senF2bGRmc2hydmh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwNjkwODYsImV4cCI6MjAwOTY0NTA4Nn0.vdZbIwkvBjROsOeXxYGz-XwqyTfAlc40OxFSu8OAjaQ";
 
 console.log(category);
 
@@ -34,9 +32,7 @@ function visMad(mad) {
     // Replace these with actual property names from your data
     copy.querySelector("img").src = item.illustrations;
     copy.querySelector("h3").textContent = item.title;
-    copy
-      .querySelector("img")
-      .setAttribute("href", `info-side.html?id=${item.illustrations}`);
+    copy.querySelector("a").setAttribute("href", `info-side.html?id=${item.id}`);
     document.querySelector("main").appendChild(copy);
   });
 }
